676550b0243cead0d55239209238e09cbbabefc0
David Green <dgreen@uab.edu>
2022-03-21 16:36:57 -0500

    Add in code to update screen and various improvements/fixes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module net.gwizlabs.airlinkinfo {
   requires javafx.controls;
   requires javafx.fxml;
   requires java.compiler;
+  requires com.fasterxml.jackson.databind;
 
   opens net.gwizlabs.airlinkinfo to
       javafx.fxml;
--- src/main/java/net/gwizlabs/airlinkinfo/PrimaryController.java
+++ src/main/java/net/gwizlabs/airlinkinfo/PrimaryController.java
@@ -1,5 +1,7 @@
 package net.gwizlabs.airlinkinfo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -10,11 +12,11 @@ public class PrimaryController {
   @FXML private TextField locationTB;
   @FXML private TextField dateTB;
   @FXML private TextField timeTB;
+  @FXML private TextField satellitesTB;
 
   // Left Panel
   @FXML private TextField typeTB0;
   @FXML private TextField availableTB0;
-  @FXML private TextField satellitesTB0;
   @FXML private TextField bandTB0;
   @FXML private TextField bandwidthTB0;
   @FXML private TextField signalStrengthTB0;
@@ -26,7 +28,6 @@ public class PrimaryController {
   // Right Panel
   @FXML private TextField typeTB1;
   @FXML private TextField availableTB1;
-  @FXML private TextField satellitesTB1;
   @FXML private TextField bandTB1;
   @FXML private TextField bandwidthTB1;
   @FXML private TextField signalStrengthTB1;
@@ -41,9 +42,134 @@ public class PrimaryController {
   @FXML private TextField upTB;
 
   @FXML
+  public void initialize() {
+    //        enableReadyButtons();
+    //        App.getStage().focusedProperty().addListener( (ObservableValue<? extends Boolean>
+    // obj,Boolean oldVal,Boolean newVal) -> {
+    //            // This listener reviews button status when
+    //            // window gets input focus.
+    //        if (newVal) { // The window has gained focus.
+    //            enableReadyButtons();
+    //        }
+    //    });
+    makeTestObject();
+  }
+
+  @FXML
   private void runTest() {
     //
   }
 
-  public void updateDisplay(AirLinkDTO info) {}
+  public void makeTestObject() {
+    String jsonInfo =
+        """
+{
+  "timestamp": {
+    "date": "03182022",
+    "time": "1453"
+  },
+
+  "version": "4.3.1.2",
+
+  "vehicleID": "~",
+
+  "location": {
+    "latitude": 35.952372,
+    "longitude": -78.964191
+  },
+
+  "wanState": [
+    {
+      "friendlyName": "Panel Ethernet 5",
+      "status": 0,
+      "active": false,
+      "networkType": "Ethernet"
+    },
+    {
+      "friendlyName": "Sierra Wireless EM75XX @ MiniCard USB3 CA (Cellular A) AT&T",
+      "status": 1,
+      "active": true,
+      "networkType": "Cellular",
+      "signalStrength": -65.000000,
+      "bandNo": "14",
+      "bandwidth": 10,
+      "RSSI": -65.000000,
+      "RSRP": -99.000000,
+      "RSRQ": -15.600000,
+      "SINR": 0.600000
+    },
+    {
+      "friendlyName": "Sierra Wireless EM75XX @ MiniCard USB3 CB (Cellular B) Verizon",
+      "status": 1,
+      "active": false,
+      "networkType": "Cellular",
+      "signalStrength": -53.909092,
+      "bandNo": "5",
+      "bandwidth": 10,
+      "RSSI": -50.000000,
+      "RSRP": -75.000000,
+      "RSRQ": -7.900000,
+      "SINR": 20.600000
+    }
+  ],
+
+  "gnssStatus": {
+    "fix": true,
+    "numberSatellites": 3
+  },
+
+  "generalInformation": {
+    "internalTemperature": 26.666667
+  }
+
+}
+    """;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    try {
+      AirLinkDTO info = objectMapper.readValue(jsonInfo, AirLinkDTO.class);
+      updateDisplay(info);
+    } catch (JsonProcessingException e) {
+      System.out.println("Error in Json Processin: " + e);
+    }
+  }
+
+  public void updateDisplay(AirLinkDTO info) {
+    locationTB.setText(
+        "" + info.getLocation().getLatitude() + ", " + info.getLocation().getLongitude());
+
+    satellitesTB.setText("" + info.getGnssStatus().getNumberSatellites());
+
+    dateTB.setText("" + info.getTimestamp().getDate());
+    timeTB.setText("" + info.getTimestamp().getTime());
+
+    var wanStates = info.getWanState();
+
+    for (WanStateDTO wanState : wanStates) {
+      if (wanState.getFriendlyName().contains("AT&T")) {
+        // AT&T
+        typeTB0.setText("" + wanState.getNetworkType());
+        availableTB0.setText("" + (wanState.getStatus() == 1 ? "Yes" : "No"));
+        bandTB0.setText("" + wanState.getBandNo());
+        bandwidthTB0.setText("" + wanState.getBandwidth());
+        signalStrengthTB0.setText("" + wanState.getSignalStrength());
+        rssiTB0.setText("" + wanState.getRssi());
+        rsrpTB0.setText("" + wanState.getRsrp());
+        rsrqTB0.setText("" + wanState.getRsrq());
+        sinrTB0.setText("" + wanState.getSinr());
+      } else if (wanState.getFriendlyName().contains("Verizon")) {
+        // Verizon
+        typeTB1.setText("" + wanState.getNetworkType());
+        availableTB1.setText("" + (wanState.getStatus() == 1 ? "Yes" : "No"));
+        bandTB1.setText("" + wanState.getBandNo());
+        bandwidthTB1.setText("" + wanState.getBandwidth());
+        signalStrengthTB1.setText("" + wanState.getSignalStrength());
+        rssiTB1.setText("" + wanState.getRssi());
+        rsrpTB1.setText("" + wanState.getRsrp());
+        rsrqTB1.setText("" + wanState.getRsrq());
+        sinrTB1.setText("" + wanState.getSinr());
+      }
+    }
+  }
 }
